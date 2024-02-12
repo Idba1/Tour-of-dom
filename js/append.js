df3252076cfaa7bd8cd2586188748afe4c37f70f
Idba1 <moniraislamself@gmail.com>
2024-02-12 13:28:32 +0600

    how to create and add element

---
+++ js/append.js
@@ -0,0 +1,9 @@
+// Where to add
+const placesList = document.getElementById("places-list");
+
+// what to be added
+const li = document.createElement("li");
+li.innerText = "pahartoli bon";
+
+// add the child
+placesList.appendChild(li);
