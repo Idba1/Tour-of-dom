a03e00a7c1528be6de68946f648e34c137545ca7
Idba1 <moniraislamself@gmail.com>
2024-02-12 14:01:22 +0600

    add a section by js

--- js/append.js
+++ js/append.js
@@ -7,3 +7,35 @@ li.innerText = "pahartoli bon";
 
 // add the child
 placesList.appendChild(li);
+
+
+
+// Where to add
+const mainContainer = document.getElementById("main-container");
+// console.log(mainContainer);
+
+// what to be added
+const section = document.createElement("section");
+const h1 = document.createElement("h1");
+h1.innerText = "My food list";
+section.appendChild(h1);
+// console.log(section);
+
+const ul = document.createElement("ul");
+
+
+const li1 = document.createElement("li");
+li1.innerText = "Biriyani";
+ul.appendChild(li1);
+
+const li2 = document.createElement("li");
+li2.innerText = "Borhani";
+ul.appendChild(li2);
+
+const li3 = document.createElement("li");
+li3.innerText = "Beresta";
+ul.appendChild(li3);
+
+
+section.appendChild(ul);
+mainContainer.appendChild(section)
